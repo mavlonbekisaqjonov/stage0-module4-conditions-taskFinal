034e19c1d2e9941ff0028f2fd472a513e65bf9f9
Mavl0nbek <mavlonbekisaqjonov222@gmail.com>
2025-01-13 11:01:17 +0500

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,91 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        boolean leap = year % 4 == 0 && ((year % 100) != 0 || (year % 400 == 0));
+        if (year <= 0) {
+            System.out.println("invalid date");
+        } else if (leap) {
+            switch (month) {
+                case 1:
+                    System.out.println("31");
+                    break;
+                case 2:
+                    System.out.println("29");
+                    break;
+                case 3:
+                    System.out.println("31");
+                    break;
+                case 4:
+                    System.out.println("30");
+                    break;
+                case 5:
+                    System.out.println("31");
+                    break;
+                case 6:
+                    System.out.println("30");
+                    break;
+                case 7:
+                    System.out.println("31");
+                    break;
+                case 8:
+                    System.out.println("31");
+                    break;
+                case 9:
+                    System.out.println("30");
+                    break;
+                case 10:
+                    System.out.println("31");
+                    break;
+                case 11:
+                    System.out.println("30");
+                    break;
+                case 12:
+                    System.out.println("31");
+                    break;
+                default:
+                    System.out.println("invalid date");
+            }
+        } else {
+            switch (month) {
+                case 1:
+                    System.out.println("31");
+                    break;
+                case 2:
+                    System.out.println("28");
+                    break;
+                case 3:
+                    System.out.println("31");
+                    break;
+                case 4:
+                    System.out.println("30");
+                    break;
+                case 5:
+                    System.out.println("31");
+                    break;
+                case 6:
+                    System.out.println("30");
+                    break;
+                case 7:
+                    System.out.println("31");
+                    break;
+                case 8:
+                    System.out.println("31");
+                    break;
+                case 9:
+                    System.out.println("30");
+                    break;
+                case 10:
+                    System.out.println("31");
+                    break;
+                case 11:
+                    System.out.println("30");
+                    break;
+                case 12:
+                    System.out.println("31");
+                    break;
+                default:
+                    System.out.println("invalid date");
+            }
+        }
     }
 }
